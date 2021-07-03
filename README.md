4d4f56f4f1ce031e9355cf7ba4d8712998a42e93
aaaa0ggmc <aaaa0ggmc@qq.com>
2021-07-03 21:42:57 +0800

    quick

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aaaa0ggMc.github.io
-website
+中文    :当你看见这个HTML页时，也许github正在处理（也许新的网站文件上传没多久）
+，请等一会再刷新，谢谢！
+English :When you see this page,maybe github is processing
+(maybe the files were uploaded just now!!!),Please wait for a while
+and the flush!
---
+++ data.js
@@ -0,0 +1,27 @@
+var pages = [
+	'<div style="text-align: center;">'+
+	'<h2>暂未选择内容呢！！！</h2>'+
+	'<img style="vertical-align: middle;" src="./smile0.jpg" />'+
+	'</div>'
+	,
+	'<h2>警察之下 UNDERPOLICE</h2>'+
+	'<p style="margin-left: 2ch;">警察之下是一个模拟类游戏，旨在让用户体验不同的生活。</p>'+
+	'<p style="text-align: center;">'+
+	'<a class="noneDec0" target="_blank" href="./UNDERPOLICE/index.html">点我访问</a>'+
+	'</p>'
+	,
+	'<h2>超度 Die</h2>'+
+	'<p style="margin-left: 2ch;">准备超度了吗？准备好了就来玩吧！</p>'+
+	'<p style="margin-left: 2ch;">注：进入页面后请等待资源加载（大约5s）......</p>'+
+	'<p style="text-align: center;">'+
+	'<a class="noneDec0" target="_blank" href="./Funning/index.html">点我访问</a>'+
+	'</p>'
+	,
+	'<h2>坏慕课 BadMooc</h2>'+
+	'<p style="margin-left: 2ch;">绝黑好慕课！！！</p>'+
+	'<p style="margin-left: 2ch;">注：别喷！！！！</p>'+
+	'<p style="text-align: center;">'+
+	'<a class="noneDec0" target="_blank" href="./BADMOOC/index.html">点我访问</a>'+
+	'</p>'
+	]
+;//Underpolice

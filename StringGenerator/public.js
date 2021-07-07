d0f7292a30a913484ff6f2002e27080fca2f006c
aaaa0ggmc <aaaa0ggmc@qq.com>
2021-07-07 18:20:27 +0800

    Videos

--- StringGenerator/public.js
+++ StringGenerator/public.js
@@ -1,10 +1,17 @@
 //Data
-console.log("ee");
 var cookie = new Cookie();
 try{
-	cookie = JSON.parse(document.cookie);
+	var str = GetCookieStrI(1);
+	if(str == null){
+		throw new DomException();
+	}
+	cookie = JSON.parse(str);
 }catch(e){
 	cookie = new Cookie();
-	document.cookie = JSON.stringify(cookie);
+	var str = GetCookieStrI(0);
+	if(str == null){
+		alert("输入Cookie失败！正在返回主界面！");
+		window.open("../index.html","_blank");
+		window.close();
+	}
 }
-console.log(cookie);
--- Tool/cookie.js
+++ Tool/cookie.js
@@ -6,6 +6,25 @@ function Cookie(){
 	this.FUG = true;//第一次用Generator
 }
 
+
+function GetCookieStrI(i){
+	var d_str = document.cookie,y;
+	for(var x = 0;x < i;x++){
+		y = d_str.indexOf(';');
+		if(y == -1){
+			return null;
+		}
+		d_str =d _str.slice(y+1);
+	}
+	y = d_str.indexOf(';');
+	if(y == -1){
+		return null;
+	}
+	d_str = d_str.slice(0,y);
+	return d_str;
+}
+
+
 //json不可储存函数！！
 function storeCookie(c){
 	document.cookie = JSON.stringify(c);
